2189e810ee4fbc039599ea53f97f4999a1761f9c
Cyril Kalikin <dck5132@gmail.com>
2016-10-20 16:11:50 -0400

    fix

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ request.open('GET', 'data.txt');
 request.onreadystatechange = function() {
 	if ((request.readyState===4) && (request.status===200))
 {
-var modify = getElementsByTagName('li');
+var modify = document.getElementsByTagName('li');
 for (var i = 0; i < modify.length; i++ )
 {
 	modify[i].innerHTML = request.responseText;
