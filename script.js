818ae05314a3af4b028f938f5944b3a52a816a2a
Cyril Kalikin <dck5132@gmail.com>
2016-10-20 15:55:21 -0400

    Second List

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ request.open('GET', 'data.txt');
 request.onreadystatechange = function() {
 	if ((request.readyState===4) && (request.status===200))
 {
-var modify = document.getElementsByTagName('li');
+var modify = document.getElementsByTagName('ul')
+[1].getElementsByTagName('li');
 modify[2].innerHTML = request.responseText;
 
 }
