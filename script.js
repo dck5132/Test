0b37cefa8925f05627e65327e536526a4ae851d9
Cyril Kalikin <dck5132@gmail.com>
2016-10-20 16:30:02 -0400

    Use child node to obtain text

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ request.open('GET', 'data.xml');
 request.onreadystatechange = function() {
 	if ((request.readyState===4) && (request.status===200))
 	{
-	console.log(request.responseXML.getElementsByTagName('name')[1]);
+	console.log(request.responseXML.getElementsByTagName('name')[1].childNodes[0]);
 		
 	}
 }
