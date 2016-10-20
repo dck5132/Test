71663b89c4aaa37a83f723b29369aa8f7ef40fcb
Cyril Kalikin <dck5132@gmail.com>
2016-10-20 14:26:28 -0400

    Added div to display results of AJAX call

--- script.js
+++ script.js
@@ -9,8 +9,9 @@ request.open('GET', 'data.txt');
 request.onreadystatechange = function() {
 	if ((request.readyState===4) && (request.status===200))
 {
-console.log(request);
-document.writeln(request.responseText);
+var modify = document.getElementById('update');
+modify.innerHTML = request.responseText;
+
 }
 }
 
