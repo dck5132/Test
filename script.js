388a16ed191f75f03d17cf35ceb9511ac86d16c7
Cyril Kalikin <dck5132@gmail.com>
2016-10-20 16:18:28 -0400

    fix 2

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ else {
 request.open('GET', 'data.txt');
 request.onreadystatechange = function() {
 	if ((request.readyState===4) && (request.status===200))
-{
-var modify = document.getElementsByTagName('li');
-for (var i = 0; i < modify.length; i++ )
-{
-	modify[i].innerHTML = request.responseText;
-}
-}
+	{
+	var modify = document.getElementsByTagName('li');
+		for (var i = 0; i < modify.length; i++ )
+		{
+			modify[i].innerHTML = request.responseText;
+		}
+	}
 }
 
 request.send();
