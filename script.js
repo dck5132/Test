67fae1df2074d55ab5aa309d6264d3d71f474f06
Cyril Kalikin <dck5132@gmail.com>
2016-10-20 16:37:49 -0400

    For loop that contains name node value of all speakers

--- script.js
+++ script.js
@@ -10,7 +10,16 @@ request.onreadystatechange = function() {
 	if ((request.readyState===4) && (request.status===200))
 	{
 	console.log(request.responseXML.getElementsByTagName('name')[1].firstChild.nodeValue);
+	
+	var items = request.responseXML.getElementsByTagName('name");
+	var output = '<ul>';
+	for (var i; i< items.length; i++){
+		output += '<li>' + items[i].firstChild.nodeValue + '</li>';
+		}
 		
+		output += '</ul>'; 
+		
+		document.getElementById('update').innerHTML = output;
 	}
 }
 
