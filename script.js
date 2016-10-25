599c2bb81867c0c4bbfa8c8f68e5608d8936a341
Cyril Kalikin <dck5132@gmail.com>
2016-10-25 15:46:56 -0400

    Added JSON version

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
-var request;
-if (window.XMLHttpRequest) {
-	request = new XMLHttpRequest();
-}
-else {
-	request = new ActiveXObject ("Microsoft.XMLHTTP");
-}
-request.open('GET', 'data.xml');
-request.onreadystatechange = function() {
-	if ((request.readyState===4) && (request.status===200))
-	{
-	console.log(request.responseXML.getElementsByTagName('name')[1].firstChild.nodeValue);
-	
-		var items = request.responseXML.getElementsByTagName('name');
-		var output = '<ul>';
-	for (var i = 0; i < items.length; i++) {
-		output += '<li>' + items[i].firstChild.nodeValue + '</li>';
-		}
-		
-		output += '</ul>';
-		
-		document.getElementById('update').innerHTML = output;
-	}
-}
+var mybutton = document.getElementById('loadbutton');
+mybutton.onclick = loadAJAX;
 
-request.send();
+function loadAJAX() {
+	var request;
+	if (window.XMLHttpRequest) {
+		request = new XMLHttpRequest();
+	}
+	else {
+		request = new ActiveXObject ("Microsoft.XMLHTTP");
+	}
+	request.open('GET', 'data.json');
+	request.onreadystatechange = function() {
+		if ((request.readyState===4) && (request.status===200))
+		{
+		var items = JSON.parse(request.responseText);
+		var output = '<ul>';
+		for (var key in items) {
+			output+='<li>' + items[key].name + '</li>';
+		}
+		output+='</ul>';
+		document.getElementById('update').innerHTML = output;
+		
+		}
+	}
+
+	request.send();
+
+}
 
