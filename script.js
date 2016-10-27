89f0a08b67a580765ddc0f476ad05a5d065dccb6
Cyril Kalikin <dck5132@gmail.com>
2016-10-27 10:24:25 -0400

    Using Jquery JSON to pass to div list of names

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 $.getJSON('data.json', function(data) {
-	console.log(data);
+	var output = '<ul>';
+	$.each(data, function(key, val) {
+		output += '<li>' + val.name + '<li>';
+	});
+	out put += '</ul>';
+	$('#update').html(output);
 });
 
 
